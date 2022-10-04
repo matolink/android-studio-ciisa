3a07de4504d2640dfa9459b39774e925fba8969b
Matías Gutiérrez <ma.gutierrez.or@gmail.com>
2022-10-04 17:54:21 -0300

    added intent from login to create new accoint

--- app/src/main/java/com/example/todociisa/Login.kt
+++ app/src/main/java/com/example/todociisa/Login.kt
@@ -1,11 +1,18 @@
 package com.example.todociisa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val login_btn_register = findViewById<TextView>(R.id.login_btn_register)
+        login_btn_register.setOnClickListener {
+            val intent = Intent(this@Login,Nueva_Cuenta::class.java)
+            startActivity(intent)
+        }
     }
 }
