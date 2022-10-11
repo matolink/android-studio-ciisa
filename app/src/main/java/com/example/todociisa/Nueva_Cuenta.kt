acd0b39f99147157765568e8048a07b325b8597e
Marcelo Cereceda <mcereceda@naturalphone.cl>
2022-10-11 19:18:28 -0300

    Validaciones

--- app/src/main/java/com/example/todociisa/Nueva_Cuenta.kt
+++ app/src/main/java/com/example/todociisa/Nueva_Cuenta.kt
@@ -1,11 +1,67 @@
 package com.example.todociisa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.todociisa.utils.Utilidades
+import com.google.android.material.textfield.TextInputLayout
 
 class Nueva_Cuenta : AppCompatActivity() {
+
+    private var username: TextInputLayout? = null
+    private var email: TextInputLayout? = null
+    private var email_confirmation: TextInputLayout? = null
+    private var password: TextInputLayout? = null
+    private var password_confirmation: TextInputLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nueva_cuenta)
+        this.setClickListenners()
+        this.setLayouts()
+    }
+
+    fun setLayouts(){
+        username = findViewById<TextInputLayout>(R.id.textInputLayout6)
+        email = findViewById<TextInputLayout>(R.id.textInputLayout9)
+        email_confirmation = findViewById<TextInputLayout>(R.id.textInputLayout7)
+        password = findViewById<TextInputLayout>(R.id.textInputLayout5)
+        password_confirmation = findViewById<TextInputLayout>(R.id.textInputLayout8)
+    }
+
+    fun setClickListenners(){
+        val button = findViewById<Button>(R.id.button5)
+        button.setOnClickListener{
+            this.onCreateButtonClick()
+        }
+        val cancelButton = findViewById<Button>(R.id.button6)
+        cancelButton.setOnClickListener{
+            finish()
+        }
+    }
+
+    fun onCreateButtonClick(){
+        if(validateForm()){
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun validateForm(): Boolean{
+        val username_text = username?.editText?.text.toString()
+        val email_text = email?.editText?.text.toString()
+        val email_confirmation_text = email_confirmation?.editText?.text.toString()
+        val password_text = password?.editText?.text.toString()
+        val password_confirmation_text = password_confirmation?.editText?.text.toString()
+
+        val utilidades = Utilidades()
+
+        return  utilidades.validateEmail(email_text, email) &&
+                utilidades.validateEmail(email_confirmation_text, email_confirmation) &&
+                utilidades.validateNull(username_text, username) &&
+                utilidades.validateNull(password_text, password) &&
+                utilidades.validateNull(password_confirmation_text, password_confirmation) &&
+                (email_confirmation_text != email_text)
     }
 }
